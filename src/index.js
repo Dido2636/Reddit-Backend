134c65c587024db82b20cf5f541e9bb78ac15d72
Do <do@MBP-de-dolores.lan>
2024-02-05 02:07:22 +0100

    home ejs

--- src/index.js
+++ src/index.js
@@ -26,9 +26,7 @@ app.use(express.static("public"));
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended : false}))
-app.get('/', (req, res) => {
-    res.json('TU AS REUSSI ')
-})
+app.get('/', (req, res) => res.render("Home"))
 
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
